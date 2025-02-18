457de35f96b2feec39022c712c67f9650bbb53cc
Santiago Lobo Ulloa <lobo09.slu@gmail.com>
2025-02-18 12:00:41 -0600

    General changes

--- pitch-arena/src/components/landing-sections/inscription.tsx
+++ pitch-arena/src/components/landing-sections/inscription.tsx
@@ -1,9 +1,17 @@
-import { Trophy, CalendarDays, CheckCircle, ArrowRight, HandCoins } from "lucide-react";
+import {
+  Trophy,
+  CalendarDays,
+  CheckCircle,
+  ArrowRight,
+  HandCoins,
+} from "lucide-react";
 
 export default function InscriptionSection() {
   return (
     <section className="w-full flex flex-col items-center py-10 px-4 sm:px-6">
-      <h2 className="text-2xl md:text-3xl font-semibold text-center">Inscripción</h2>
+      <h2 className="text-2xl md:text-3xl font-semibold text-center">
+        Inscripción
+      </h2>
       <p className="text-gray-500 text-center mt-2 text-sm md:text-base">
         Asegura tu lugar para participar y presentar tu idea innovadora
       </p>
@@ -30,9 +38,14 @@ export default function InscriptionSection() {
               <strong>Tercer lugar:</strong> 10,000 colones
             </li>
           </ul>
-          <button className="mt-4 w-full sm:w-[12rem] flex items-center justify-center gap-2 bg-[#246AAF] text-white p-3 rounded-full text-sm md:text-[15px] font-medium transition">
+          <a
+            href="https://forms.office.com/r/eUZVUXv6nk?origin=lprLink"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mt-4 w-full sm:w-[12rem] flex items-center justify-center gap-2 bg-[#246AAF] text-white p-3 rounded-full text-sm md:text-[15px] font-medium transition hover:bg-lightGray hover:text-blue cursor-pointer"
+          >
             Inscribirse ahora <ArrowRight className="w-4 h-4 md:w-5 md:h-5" />
-          </button>
+          </a>
         </article>
 
         <article className="flex flex-col justify-between bg-gray-100 rounded-xl p-6 sm:p-8 shadow-md w-full lg:w-1/2">
@@ -46,9 +59,20 @@ export default function InscriptionSection() {
             <CalendarDays className="w-5 h-5 md:w-6 md:h-6 text-gray-500" />
             Lee las fechas e indicaciones para asistir.
           </p>
-          <p className="text-gray-700 mt-2 flex items-center gap-3 text-sm md:text-base">
+          <p className="text-gray-700 mt-2 flex flex-wrap items-center gap-2 text-sm md:text-base">
             <ArrowRight className="w-5 h-5 md:w-6 md:h-6 text-gray-500" />
-            Entra a EventBridge y confirma tu asistencia.
+            <span className="inline-flex items-center gap-2">
+              Entra a
+              <a
+                href="https://www.eventbrite.es/e/summit-de-innovacion-emprendimiento-y-sostenibilidad-startup-supernovas-tickets-1111628935069"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-600 font-medium hover:text-blue-700 transition-colors"
+              >
+                Eventbrite
+              </a>
+              y confirma tu asistencia.
+            </span>
           </p>
         </article>
       </div>
--- pitch-arena/src/components/landing-sections/judges.tsx
+++ pitch-arena/src/components/landing-sections/judges.tsx
@@ -1,18 +1,17 @@
 import { JudgeInfo } from '../ui/JudgeInfo';
 export const Judges = () => {
   return (
-    <section className="w-full flex flex-col items-center py-6 px-4 sm:px-6">
+    <section className="w-full flex flex-col items-center py-16 px-4 sm:px-6">
       <h2 className="text-2xl md:text-3xl font-semibold text-center">Jueces</h2>
       <p className="text-gray-500 text-center mt-2 text-sm md:text-base">
         Conoce más de las personas que te van a evaluar{" "}
       </p>
-      <div className="mt-8 flex flex-col gap-8 items-center justify-center lg:flex-row lg:gap-12 w-full max-w-6xl">
-        <article className="flex flex-col h-full gap-6 justify-center items-center rounded-xl px-6 sm:px-8 w-full">
-          <div className="w-full flex flex-col md:flex-row items-center justify-center gap-5">
-            <div className='flex flex-col md:flex-row md:gap-14'>
-                  <JudgeInfo judgeName={"Name 1"}/>
-                  <JudgeInfo judgeName={'Name 2'}/>
-                  <JudgeInfo judgeName={'Name 3'}/>
+      <div className="mt-8 flex flex-col gap-8 items-center justify-center lg:flex-row lg:gap-12">
+        <article className="flex flex-col h-full justify-center items-center rounded-xl px-6 sm:px-8 w-full">
+          <div className="w-full flex flex-col md:flex-row items-center justify-center">
+            <div className='flex flex-col md:flex-row gap-12'>
+                  <JudgeInfo judgeName={"Briansy Angulo Gaucherand"} judgeDescription={"Coordinador de Innovación"} judgePhoto={"/images/Judge1.jpeg"}/>
+                  <JudgeInfo judgeName={'Mariana Solano'} judgePhoto={"/images/judge2.jpg"} judgeDescription={"Gestora de Innovación en Parque Tec"}/>
             </div>
           </div>
         </article>
